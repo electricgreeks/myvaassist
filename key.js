ff1fd19968febac8a3bf1a7ab0186c2df82407e1
electricgreeks <jrfmick@yahoo.com>
2026-05-06 03:17:12 -0500

    Add files via upload

---
+++ key.js
@@ -0,0 +1,5 @@
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  return res.status(200).json({ k: process.env.ANTHROPIC_API_KEY });
+};
